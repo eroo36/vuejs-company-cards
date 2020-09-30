71ac7e675a43990cbe435df366bc7a94edc16f15
Eroo36 <edogruca@hotmail.com>
2020-09-30 18:03:07 +0300

    first commit

--- src/views/EditCompany.vue
+++ src/views/EditCompany.vue
@@ -6,17 +6,19 @@
 		<div style="margin: 20px;">
 			<form id="edit-company-form" @submit="submitEvent">
 				<div class="grid-container">
-					<div @change="setCompanyName" class="item1">
+					<div @change="setCompanyName" style="text-align: center;" >
 						Company Name
 						<input
-						style="width: 36%"
+            class="input-group"
+						style=""
 							:placeholder="getCompanyObj[0].companyName"
 						>
 					</div>
-					<div v-on:change="minScope" class="item1">
+					<div v-on:change="minScope" style="text-align: center; margin-top: 15px;" >
 						Company Min Spend
 						<input
-						style="width: 36%"
+						style="width: 30%; margin-left: auto; margin-right: auto; margin-top: 15px;"
+             class="input-group"
 							:placeholder="getCompanyObj[0].minSpend"
 							min="0"
 							step="0.1"
@@ -24,10 +26,11 @@
 							type="number"
 						>
 					</div>
-					<div v-on:change="maxScope" class="item1">
+					<div v-on:change="maxScope" style="text-align: center; margin-top: 15px;" >
 						Company Max Spend
 						<input
-						style="width: 36%"
+						style="width: 30%; margin-left: auto; margin-right: auto;"
+             class="input-group"
 							:placeholder="getCompanyObj[0].maxSpend"
 							:min="minSpend"
 							step="0.1"
@@ -35,7 +38,7 @@
 							type="number"
 						>
 					</div>
-					<div class="item2">
+					<div class="item2" style="">
 					<h3>Additional Notes</h3>
 					<textarea
 						@click="textAreaClick"
@@ -47,8 +50,9 @@
 				</div>
 				<div id="myModal" class="modal">
 					<div class="modal-content">
-						<span @click="closeModal" class="close">&times;</span>
+						<span @click="closeModal" class="close" >&times;</span>
 						<textarea
+            class="text-span"
 							id=""
 							cols="145"
 							rows="10"
@@ -178,24 +182,51 @@ export default {
 }
 
 .grid-container {
-  display: grid;
-  grid-template-columns: auto auto auto auto;
-  grid-gap: 10px;
+  display: column;
+  grid-template-columns: auto auto auto;
   padding: 10px;
 }
-
-.grid-container > div {
+.grid-item {
   background-color: rgba(255, 255, 255, 0.8);
-  text-align: right;
-  padding: 20px 0;
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
   font-size: 30px;
+  text-align: center;
 }
 
-.item1 {
-  grid-column: 1 / span 2;
+.input-group {
+  width: 30%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 15px;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: right;
+  align-items: right;
+  padding: 0.375rem 0.75rem;
+  margin-bottom: 0;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  text-align: start;
+  white-space: nowrap;
+  background-color: #e9ecef;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
 }
+
 .item2 {
-  grid-column: 1 / span 2;
-  text-align: right !important;
+  width: 30%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 15px;
+  text-align: center;
+}
+.text-span {
+  width: 50%;
+  margin-left: 25%;
+  margin-top: 15px;
+  text-align: center;
 }
 </style>
